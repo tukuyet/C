5b564587e8fc7ee93c587df1c3cc4f2ad3d1591a
tukuyet <103932488+tukuyet@users.noreply.github.com>
2022-08-06 21:32:12 +0700

    thi thuc hanh

---
+++ thi thuc hanh/ConsoleApp1/bai1/Program.cs
@@ -0,0 +1,21 @@
+﻿class Cylinder
+{
+    static void Main(String[] args)
+    {
+        //Process
+        Console.WriteLine("Enter the dimenstions of the cylinder");
+        Console.Write("Radius : ");
+        double Radius = double.Parse(Console.ReadLine());
+        Console.Write("Height : ");
+        double Height = double.Parse(Console.ReadLine());
+
+        //Result
+        Console.WriteLine("\nCylinder Characteristics");
+        Console.WriteLine("Radius: " + Radius + ", Height: " + Height);
+        double BaseArea = Radius * Radius * Math.PI;
+        double LateralArea = 2 * Math.PI * Radius * Height;
+        double TotalArea = 2 * Math.PI * Radius * (Height + Radius);
+        double Volume = Math.PI * Radius * Radius * Height;
+        Console.WriteLine("Base: " + BaseArea + " | Latera: " + LateralArea + " | Total: " + TotalArea + " | Volume: " + Volume);
+    }
+}
---
+++ thi thuc hanh/ConsoleApp2/bai2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+class Animal
+{
+    public string name;
+    public double weight;
+    public void Show()
+    {
+        Console.WriteLine("Name: " + name + ", Weight: " + weight);
+
+    }
+    public void SetMe(string name, double weight)
+    {
+        this.name = name;
+        this.weight = weight;
+    }
+}
+
+class Lion : Animal
+{
+    public Lion(string name, double weight)
+    {
+        SetMe(name, weight);
+        Show();
+    }
+
+}
+
+class Tiger : Animal
+{
+    public Tiger(string name, double weight)
+    {
+        SetMe(name, weight);
+        Show();
+    }
+
+}
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Lion lion = new Lion("lion", 100);
+        Tiger tiger = new Tiger("tiger", 200);
+        Console.WriteLine();
+    }
+}
