bf7f8f14c1617f7639ce13f7e42b45dcd26a3369
tukuyet <103932488+tukuyet@users.noreply.github.com>
2022-08-06 21:24:33 +0700

    r

---
+++ project/Program.cs
@@ -0,0 +1,17 @@
+﻿class Program
+{
+    public static void Main()
+    {
+        List<Product> inv = new List<Product>();
+        // Add elements to the list
+        inv.Add(new Product("A", 5.9, 3));
+        inv.Add(new Product("B", 8.2, 2));
+        inv.Add(new Product("C", 3.5, 4));
+        inv.Add(new Product("D", 1.8, 8));
+        Console.WriteLine("Product list:");
+        foreach (Product i in inv)
+        {
+            Console.WriteLine(" " + i);
+        }
+    }
+}
